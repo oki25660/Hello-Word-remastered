27e21775cd4ccc08b8e29cbe84801d4f8fe1b5f0
OKI <OKI@PCPRO>
2021-10-16 23:20:19 +0200

    Ajoutez des fichiers projet.

---
+++ Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("                                                    * * * * * * * * *")
+        Console.WriteLine("                                                    *  Hello World! *")
+        Console.WriteLine("                                                    * * * * * * * * *")
+        Console.WriteLine("")
+        Console.WriteLine("                                                     CREATE BY ITHEC ")
+    End Sub
+End Module
